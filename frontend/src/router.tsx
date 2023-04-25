97c2ce8d4bd7370096c562e49da54af1bab61acd
정상기 <jackid1103@naver.com>
2023-04-25 10:47:58 +0900

    ✨ feat : 챕터별 children 라우팅 적용

--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -1,10 +1,23 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "./pages/AppPage";
 import ProjectStepPage from "./pages/ProjectStepPage";
+import InputSection1 from "@components/CreateProject/RightSection/InputSection1";
+import InputSection2 from "@components/CreateProject/RightSection/InputSection2";
+import InputSection3 from "@components/CreateProject/RightSection/InputSection3";
+import InputSection4 from "@components/CreateProject/RightSection/InputSection4";
 
 const router = createBrowserRouter([
   { path: "/", element: <App /> },
-  { path: "/step", element: <ProjectStepPage /> },
+  {
+    path: "/step",
+    element: <ProjectStepPage />,
+    children: [
+      { path: "1", element: <InputSection1 />, index: true },
+      { path: "2", element: <InputSection2 /> },
+      { path: "3", element: <InputSection3 /> },
+      { path: "4", element: <InputSection4 /> },
+    ],
+  },
 ]);
 
 export default router;
