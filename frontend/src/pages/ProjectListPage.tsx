73ff321267461f9fe3917de5daac6347e7e22e66
aldls819 <aldls819@gmail.com>
2023-04-27 00:51:51 +0900

    :lipstick: Design: css 수정 중

--- frontend/src/pages/ProjectListPage.tsx
+++ frontend/src/pages/ProjectListPage.tsx
@@ -11,30 +11,24 @@ export default function ProjectListPage() {
   return (
     <>
       <Header />
-      <SWrapper>
-        <STitleBox>
-          <STitle>프로젝트 목록</STitle>
-        </STitleBox>
-        <SListBox>
-          <SListTitleDiv>
-            <SListTitle>프로젝트 명</SListTitle>
-            <SListTitle>아이템 수</SListTitle>
-            <SListTitle>최근 빌드 성공</SListTitle>
-            <SListTitle>최근 빌드 실패</SListTitle>
-          </SListTitleDiv>
-          <ProjectList />
-        </SListBox>
-      </SWrapper>
+      <STitleBox>
+        <STitle>프로젝트 목록</STitle>
+      </STitleBox>
+      <SProjectList></SProjectList>
     </>
   );
 }
 
+const SProjectName = styled.div`
+  flex-direction: row;
+`;
+
 const STitleBox = styled.div`
-  width: 70vw;
+  width: 75vw;
   height: 15vh;
   background-color: white;
   border-radius: 3rem;
-  margin: 3vh auto 3vh;
+  margin: 2vh auto;
   display: flex;
   justify-content: left;
   align-items: center;
@@ -47,27 +41,11 @@ const STitle = styled.p`
   margin-left: 3vh;
 `;
 
-const SListBox = styled.div`
-  width: 70vw;
-  height: 65vh;
+const SProjectList = styled.div`
+  width: 75vw;
+  height: 70vh;
   background-color: white;
   border-radius: 3rem;
-  margin: 0px auto 3vh;
-  overflow: auto;
-`;
-
-const SListTitle = styled.p`
-  text-align: center;
-  font-size: 2rem;
-  font-weight: 800;
-`;
-
-const SListTitleDiv = styled.div`
+  margin: 3vh auto;
   display: flex;
-  flex-wrap: wrap;
-  margin-top: 1vh;
-  max-width: 65vw;
-  justify-content: space-evenly;
 `;
-
-const SWrapper = styled.div``;
