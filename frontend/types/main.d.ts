eefbcc6760b6e168c90cc102b25f409e35777fc1
정상기 <jackid1103@naver.com>
2023-04-26 09:21:00 +0900

    ✨ feat : 프로젝트 생성 type 추가

--- frontend/types/main.d.ts
+++ frontend/types/main.d.ts
@@ -3,3 +3,44 @@ interface IStepItem {
   desc: String;
   status: String;
 }
+
+interface IProject {
+  itemList: itemList[];
+  gitConfig: IGitConfig;
+  nginxConfig: INginxConfig;
+
+  projectName: string;
+  description: string;
+  imageUrl: string;
+}
+
+interface IItemList {
+  itemName: string;
+  portNumber1: number;
+  portNumber2: number;
+  branchName: string;
+  secretToken: string;
+  targetFolder: string;
+  frameworkType: string;
+  buildVersion: string;
+}
+
+interface IGitConfig {
+  hostUrl: string;
+  repositoryUrl: string;
+  projectId: string;
+  credentialId: string;
+  accessToken: string;
+}
+
+interface INginxConfig {
+  domainUrl: string;
+  sslCertificate: string;
+  sslCertificateKey: string;
+  proxyPathList: IProxyPath[];
+}
+
+interface IProxyPath {
+  pathUrl: stirng;
+  pathName: stirng;
+}
