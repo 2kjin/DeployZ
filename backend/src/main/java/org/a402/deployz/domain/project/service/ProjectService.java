5fc39135a1be99f1171a230d9485f4ed07a21992
eunjikim8784 <eunjikim8784@gmail.com>
2023-04-28 15:17:37 +0900

    💡 refactor: 프로젝트 생성 request 데이터 형식 수정
    GitConfigRequest를 ProjectConfigRequest로 합치고 관련 로직 수정함

--- backend/src/main/java/org/a402/deployz/domain/project/service/ProjectService.java
+++ backend/src/main/java/org/a402/deployz/domain/project/service/ProjectService.java
@@ -1,16 +1,17 @@
 package org.a402.deployz.domain.project.service;
 
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+import static org.a402.deployz.domain.project.entity.enums.FrameworkType.*;
+import static org.a402.deployz.domain.project.entity.enums.ReactVersion.*;
+import static org.a402.deployz.domain.project.entity.enums.SpringBootVersion.*;
+
+import java.util.List;
+
 import org.a402.deployz.domain.git.entity.GitConfig;
 import org.a402.deployz.domain.git.entity.GitToken;
 import org.a402.deployz.domain.member.entity.Member;
 import org.a402.deployz.domain.member.repository.MemberRepository;
 import org.a402.deployz.domain.project.entity.NginxConfig;
 import org.a402.deployz.domain.project.entity.Project;
-import org.a402.deployz.domain.project.entity.enums.FrameworkType;
 import org.a402.deployz.domain.project.exception.ProjectNotFoundException;
 import org.a402.deployz.domain.project.repository.GitConfigRepository;
 import org.a402.deployz.domain.project.repository.GitTokenRepository;
@@ -25,11 +26,8 @@ import org.a402.deployz.global.error.GlobalErrorCode;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
-
-import static org.a402.deployz.domain.project.entity.enums.FrameworkType.*;
-import static org.a402.deployz.domain.project.entity.enums.ReactVersion.getReactVersion;
-import static org.a402.deployz.domain.project.entity.enums.SpringBootVersion.getSpringBootVersion;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 //  | findOrder() | 조회 유형의 service 메서드 |
 //  | addOrder() | 등록 유형의 service 메서드 |
@@ -41,72 +39,72 @@ import static org.a402.deployz.domain.project.entity.enums.SpringBootVersion.get
 @Slf4j
 public class ProjectService {
 
-    private final ProjectRepository projectRepository;
-    private final MemberRepository memberRepository;
-    private final GitConfigRepository gitConfigRepository;
-    private final NginxConfigRepository nginxConfigRepository;
-    private final ProxyConfigRepository proxyConfigRepository;
-    private final ItemRepository itemRepository;
-    private final GitTokenRepository gitTokenRepository;
+	private final ProjectRepository projectRepository;
+	private final MemberRepository memberRepository;
+	private final GitConfigRepository gitConfigRepository;
+	private final NginxConfigRepository nginxConfigRepository;
+	private final ProxyConfigRepository proxyConfigRepository;
+	private final ItemRepository itemRepository;
+	private final GitTokenRepository gitTokenRepository;
 
-    @Transactional
-    public void addProject(TotalProjectConfigRequest request) {
-        // Project 저장
-        // @FIXME: need token parsing
-        Member member = memberRepository.findMemberByEmail("eunjikim8784@gmail.com");
-        Project project = projectRepository.save(
-                request.getProjectConfig().toEntity(member));
+	@Transactional
+	public void addProject(TotalProjectConfigRequest request) {
+		// Project 저장
+		// @FIXME: need token parsing
+		Member member = memberRepository.findMemberByEmail("eunjikim8784@gmail.com");
+		Project project = projectRepository.save(
+			request.getProjectConfig().toEntity(member));
 
-        // GitConfig 저장
-        GitConfig gitConfig = gitConfigRepository.save(request.getGitConfig().toEntity(project));
+		// GitConfig 저장
+		GitConfig gitConfig = gitConfigRepository.save(request.getProjectConfig().toGEntity(project));
 
-        // Item 저장
-        for (int i = 0; i < request.getItemList().size(); i++) {
-            ItemConfigRequest itemConfigRequest = request.getItemList().get(i);
-            itemRepository.save(itemConfigRequest.toEntity(project));
+		// Item 저장
+		for (int i = 0; i < request.getItemList().size(); i++) {
+			ItemConfigRequest itemConfigRequest = request.getItemList().get(i);
+			itemRepository.save(itemConfigRequest.toEntity(project));
 
-            // GitToken 저장
-            GitToken gitToken = GitToken.builder()
-                    .secretToken(itemConfigRequest.getSecretToken())
-                    .branchName(itemConfigRequest.getBranchName())
-                    .gitConfig(gitConfig)
-                    .build();
+			// GitToken 저장
+			GitToken gitToken = GitToken.builder()
+				.secretToken(itemConfigRequest.getSecretToken())
+				.branchName(itemConfigRequest.getBranchName())
+				.gitConfig(gitConfig)
+				.build();
 
-            gitTokenRepository.save(gitToken);
-        }
+			gitTokenRepository.save(gitToken);
+		}
 
-        // NginxConfig 저장
-        NginxConfigRequest nginxConfigRequest = request.getNginxConfig();
-        NginxConfig nginxConfig = nginxConfigRepository.save(nginxConfigRequest.toEntity(project));
+		// NginxConfig 저장
+		NginxConfigRequest nginxConfigRequest = request.getNginxConfig();
+		NginxConfig nginxConfig = nginxConfigRepository.save(nginxConfigRequest.toEntity(project));
 
-        // ProxyConfig 저장
-        for (int i = 0; i < nginxConfigRequest.getProxyPathList().size(); i++) {
-            proxyConfigRepository.save(nginxConfigRequest.getProxyPathList().get(i).toEntity(nginxConfig));
-        }
-    }
+		// ProxyConfig 저장
+		for (int i = 0; i < nginxConfigRequest.getProxyPathList().size(); i++) {
+			proxyConfigRepository.save(nginxConfigRequest.getProxyPathList().get(i).toEntity(nginxConfig));
+		}
+	}
 
-    @Transactional
-    public void removeProject(long idx) {
-        projectRepository.findByIdx(idx)
-                .orElseThrow(() -> new ProjectNotFoundException(GlobalErrorCode.PROJECT_NOT_FOUND))
-                .updateDeletedFlag();
-    }
+	@Transactional
+	public void removeProject(long idx) {
+		projectRepository.findByIdx(idx)
+			.orElseThrow(() -> new ProjectNotFoundException(GlobalErrorCode.PROJECT_NOT_FOUND))
+			.updateDeletedFlag();
+	}
 
-    @Transactional
-    public List<String> findFrameworkTypeList() {
-        return getFrameworkNames();
-    }
+	@Transactional
+	public List<String> findFrameworkTypeList() {
+		return getFrameworkNames();
+	}
 
-    public List<String> findBuildVersionList(String frameworkType) {
-        List<String> names = null;
+	public List<String> findBuildVersionList(String frameworkType) {
+		List<String> names = null;
 
-        if (frameworkType.equals(REACT.getName())) {
-            names = getReactVersion();
-        } else if (frameworkType.equals(SPRINGBOOT.getName())) {
-            names = getSpringBootVersion();
-        }
+		if (frameworkType.equals(REACT.getName())) {
+			names = getReactVersion();
+		} else if (frameworkType.equals(SPRINGBOOT.getName())) {
+			names = getSpringBootVersion();
+		}
 
-        return names;
-    }
+		return names;
+	}
 
 }
