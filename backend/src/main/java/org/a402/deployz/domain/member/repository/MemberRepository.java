6f1b728fedb56f086d08dc916c9118ca5dcaa35a
alkwen0996 <alkwen0996@naver.com>
2023-04-29 19:31:44 +0900

    💡 refactor: MemberRepository findMemberByEmail 반환형 수정

--- backend/src/main/java/org/a402/deployz/domain/member/repository/MemberRepository.java
+++ backend/src/main/java/org/a402/deployz/domain/member/repository/MemberRepository.java
@@ -1,11 +1,12 @@
 package org.a402.deployz.domain.member.repository;
 
+import java.util.Optional;
+
 import org.a402.deployz.domain.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
-
-  Member findMemberByEmail(final String email);
+	Optional<Member> findMemberByEmail(final String email);
 }
