647a1c10df713215258bc8a4c50b036b69d6deb3
Minji Park <pmj7755@naver.com>
2023-05-01 15:53:59 +0900

    refactor: security ë¥에서 사용하지 않는 import 삭제

--- backend/src/main/java/org/a402/deployz/global/security/config/SecurityConfig.java
+++ backend/src/main/java/org/a402/deployz/global/security/config/SecurityConfig.java
@@ -2,9 +2,6 @@ package org.a402.deployz.global.security.config;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
 import org.a402.deployz.global.security.jwt.JwtAuthenticationFilter;
 import org.a402.deployz.global.security.jwt.JwtTokenProvider;
 import org.a402.deployz.global.security.oauth.CustomOAuth2UserService;
