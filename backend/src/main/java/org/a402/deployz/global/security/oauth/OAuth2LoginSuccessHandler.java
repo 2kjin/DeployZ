92e03920241135527d13656707191f7492e18fa5
alkwen0996 <alkwen0996@naver.com>
2023-05-01 15:43:31 +0900

    💡 refactor: 로그인 성공 후 redirect 경로 수정

--- backend/src/main/java/org/a402/deployz/global/security/oauth/OAuth2LoginSuccessHandler.java
+++ backend/src/main/java/org/a402/deployz/global/security/oauth/OAuth2LoginSuccessHandler.java
@@ -35,8 +35,8 @@ import lombok.extern.slf4j.Slf4j;
 public class OAuth2LoginSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
 	public static final String REGISTRATION_ID = "registrationId";
 	//	public static final String SERVER_REDIRECT_URL = "/socialLogin";
-	//	public static final String SERVER_REDIRECT_URL = "https://j8a602.p.ssafy.io/socialLogin";
-	public static final String SERVER_REDIRECT_URL = "http://localhost:3000/login";
+	//	public static final String SERVER_REDIRECT_URL = "https://j8a602.p.ssafy.io/oauthRedirect";
+	public static final String SERVER_REDIRECT_URL = "http://localhost:5173/oauthRedirect";
 	private final JwtTokenProvider jwtTokenProvider;
 	private final MemberRepository memberRepository;
 	private final HttpCookieOAuthAuthorizationRequestRepository httpCookieOAuthAuthorizationRequestRepository;
