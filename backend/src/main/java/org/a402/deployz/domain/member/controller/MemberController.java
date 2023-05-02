cad805da55dd5210d74b79d741e67fdb6026769f
alkwen0996 <alkwen0996@naver.com>
2023-05-02 17:24:58 +0900

    💡 refactor: 유저정보 조회 api 파라미터 수정

--- backend/src/main/java/org/a402/deployz/domain/member/controller/MemberController.java
+++ backend/src/main/java/org/a402/deployz/domain/member/controller/MemberController.java
@@ -6,12 +6,13 @@ import org.a402.deployz.domain.member.response.MemberInformationResponse;
 import org.a402.deployz.domain.member.response.MemberUpdateResponse;
 import org.a402.deployz.domain.member.service.MemberService;
 import org.a402.deployz.global.common.BaseResponse;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.v3.oas.annotations.Operation;
@@ -32,8 +33,8 @@ public class MemberController {
 	@ApiResponse(responseCode = "200", description = "유저정보 불러오기 성공")
 	@ApiResponse(responseCode = "400", description = "유저정보 찾을 수 없음")
 	@GetMapping
-	public BaseResponse<MemberInformationResponse> findMemberInfo(@RequestParam Long idx) {
-		final MemberInformationResponse memberInformation = memberService.findMemberInformation(idx);
+	public BaseResponse<MemberInformationResponse> findMemberInfo(@AuthenticationPrincipal final UserDetails userDetails) {
+		final MemberInformationResponse memberInformation = memberService.findMemberInformation(userDetails);
 
 		return new BaseResponse<>(memberInformation);
 	}
