bd99dbfaa7414e95ef25f73ac76fba7431433c72
alkwen0996 <alkwen0996@naver.com>
2023-05-04 17:32:49 +0900

    💡 refactor: filter 허용권한 수정

--- backend/src/main/java/org/a402/deployz/global/security/config/SecurityConfig.java
+++ backend/src/main/java/org/a402/deployz/global/security/config/SecurityConfig.java
@@ -62,7 +62,7 @@ public class SecurityConfig {
 
 		httpSecurity.authorizeRequests()
 			//HttpServletRequest를 사용하는 요청에 대한 권한체크
-			.antMatchers("/").permitAll().anyRequest().authenticated();
+			.antMatchers("/", "/actuator/health").permitAll().anyRequest().authenticated();
 
 		httpSecurity.authorizeRequests()// PERMIT_URL_ARRAY 에서 지정한 인증없이 권한 허가.
 			.and()
