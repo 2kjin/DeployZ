c75a665624292576c9d5d6c882306c0cd3f3ad72
lkjin1202 <lkjin1202@gmail.com>
2023-05-10 17:35:14 +0900

    ✨ feat : camera 움직임 센터 조정

--- frontend/src/components/Landing/Intro1.tsx
+++ frontend/src/components/Landing/Intro1.tsx
@@ -1,15 +1,14 @@
 import styled from "styled-components";
-import { theme } from "@/styles/theme"
+import { theme } from "@/styles/theme";
 import Intro1Compo from "./Intro1Compo";
 import { useRef } from "react";
-import ZoomInOut from "./Intro1Compo copy";
 
 export default function Intro1() {
   const size = useRef<HTMLDivElement>(null);
   return (
     <Container>
       <div ref={size} className="left-container">
-        <ZoomInOut size={size} />
+        <Intro1Compo size={size} />
       </div>
       <div className="right-container"></div>
     </Container>
--- frontend/src/components/Landing/Intro1Compo.tsx
+++ frontend/src/components/Landing/Intro1Compo.tsx
@@ -3,37 +3,40 @@ import { useEffect } from "react";
 import styled from "styled-components";
 import { theme } from "@/styles/theme";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
-import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
-import GlbFile from "@/assets/3dmodel/factory.glb"
+import { DRACOLoader } from "three/examples/jsm/loaders/DRACOLoader";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import GlbFile from "@/assets/3dmodel/factory.glb";
 
-export default function Intro1Compo({size} : {size: React.MutableRefObject<HTMLDivElement>}) {
-
+export default function Intro1Compo({
+  size,
+}: {
+  size: React.MutableRefObject<HTMLDivElement>;
+}) {
   useEffect(() => {
     // camera
     const camera = new THREE.PerspectiveCamera(
       70,
       size.current.clientWidth / size.current.clientHeight,
       0.1,
-      1000,
-      );
-      camera.position.z = 100;
-      
-      // scene
+      1000
+    );
+    camera.position.z = 100;
+
+    // scene
     const scene = new THREE.Scene();
     scene.background = new THREE.Color(`${theme.colors.container}`);
-    
+
     //renderer
     const renderer = new THREE.WebGLRenderer({ antialias: true });
-    renderer.setSize(size.current.clientWidth,size.current.clientHeight);
+    renderer.setSize(size.current.clientWidth, size.current.clientHeight);
     document.getElementById("container").appendChild(renderer.domElement);
-    
+
     // set mesh
     // const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
     // const material = new THREE.MeshBasicMaterial({ color: `${theme.colors.primary}` });
     // const mesh = new THREE.Mesh(geometry, material);
     // scene.add(mesh);
-    
+
     //Three.js에서 제공하는 카메라 컨트롤러 클래스
     const controls = new OrbitControls(camera, renderer.domElement);
     controls.target.set(0, 0.5, 0); // 카메라의 시선
@@ -41,22 +44,27 @@ export default function Intro1Compo({size} : {size: React.MutableRefObject<HTMLD
 
     // glTF 파일에서 압축된 데이터를 디코딩
     const dracoLoader = new DRACOLoader();
-    dracoLoader.setDecoderPath('jsm/libs/draco/gltf/'); // DRACO 디코더 파일의 경로
-    
+    dracoLoader.setDecoderPath("jsm/libs/draco/gltf/"); // DRACO 디코더 파일의 경로
+
     // 에셋 추가
     const loader = new GLTFLoader();
-    loader.setDRACOLoader( dracoLoader );
+    loader.setDRACOLoader(dracoLoader);
     loader.load(GlbFile, (gltf) => {
       const model = gltf.scene;
 
       // 로드된 모델의 위치, 크기, 회전 설정
-      model.position.set(50, 0, 0);
+      model.position.set(0, 0, 0);
       model.scale.set(7, 7, 7);
-      model.rotation.set(0, 2, 0);
-      
+      model.rotation.set(0, -2, 0);
+
+      const bbox = new THREE.Box3().setFromObject(model);
+      const center = new THREE.Vector3();
+      bbox.getCenter(center);
+      model.position.sub(center);
+
       // scene에 모델 추가
-      scene.add(model)
-    })
+      scene.add(model);
+    });
 
     function animate() {
       requestAnimationFrame(animate);
@@ -74,4 +82,4 @@ const Container = styled.div`
   justify-content: center;
   width: 100vw;
   height: 100vh;
-  `
+`;
