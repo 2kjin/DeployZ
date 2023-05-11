5a91221cfbcc1d734681504e170e1eb7e3383b23
alkwen0996 <alkwen0996@naver.com>
2023-05-11 10:49:35 +0900

    💡 refactor: filter 인증 url 수정

--- backend/src/main/java/org/a402/deployz/global/security/config/SecurityConfig.java
+++ backend/src/main/java/org/a402/deployz/global/security/config/SecurityConfig.java
@@ -3,6 +3,8 @@ package org.a402.deployz.global.security.config;
 import java.util.Arrays;
 import java.util.List;
 
+import org.a402.deployz.global.security.jwt.JwtAuthenticationEntryPoint;
+import org.a402.deployz.global.security.jwt.JwtAuthenticationFilter;
 import org.a402.deployz.global.security.jwt.JwtTokenProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,6 +13,7 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -20,9 +23,6 @@ import lombok.RequiredArgsConstructor;
 @Configuration
 @RequiredArgsConstructor
 public class SecurityConfig {
-	public static final String SERVER_URL_PATTERN = "http://k8a402.p.ssafy.io";
-	public static final String FRONT_URL_PATTERN = "http://localhost:5173";
-	public static final String FRONT_URL_PATTERN_SERVER = "http://localhost:3000";
 	public static final String BASE_URL_PATTERN = "/**";
 	public static final String HEAD = "HEAD";
 	public static final String OPTIONS = "OPTIONS";
@@ -31,27 +31,42 @@ public class SecurityConfig {
 	public static final String DELETE = "DELETE";
 	public static final String PUT = "PUT";
 	public static final String ALLOW_PATTERN = "*";
+	private static final String[] PERMIT_URL_ARRAY = {
+		/*회원가입*/
+		"/api/member/validateServerKey",
+		"/api/member/signup",
+		"/api/member/login",
+		"/",
+		"/actuator/**",
+	};
 
 	private final JwtTokenProvider jwtTokenProvider;
+	private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
 
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
 		httpSecurity
 			.httpBasic().disable() // rest api 이기 때문에 기본 설정을 사용하지 않는다. (기본설정은 비인증시 로그인 화면으로 리다이렉트)
 			.csrf().disable() // rest api 이기 때문에 csrf 보안이 필요 없으므로 disable 처리한다.
-			//			.formLogin().disable()
+			.cors().configurationSource(corsConfigurationSource())
+			.and()
 			.sessionManagement()
-			.sessionCreationPolicy(SessionCreationPolicy.STATELESS) // jwt Token으로 인증하므로 session 생성하지 않는다.
-			.and()
-			.cors().configurationSource(corsConfigurationSource());
+			.sessionCreationPolicy(SessionCreationPolicy.STATELESS); // jwt Token으로 인증하므로 session 생성하지 않는다.
 
-		//HttpServletRequest를 사용하는 요청에 대한 권한체크
-		httpSecurity.authorizeRequests()
-			.antMatchers("*")
+		httpSecurity
+			.authorizeRequests()
+			.antMatchers("/api/project/**", "/api/item/**", "/api/git/**")
+			.authenticated()
+			.antMatchers(PERMIT_URL_ARRAY)
 			.permitAll()
+			.anyRequest()
+			.permitAll();
+
+		//HttpServletRequests를 사용하는 요청에 대한 권한체크
+		httpSecurity
+			.authorizeRequests()
 			.and()
-		//			.addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class)
-		;
+			.addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class);
 		// 커스텀 필터를 ID/PW 기반으로 인증하는 기본 필터 앞에 넣어서 먼저 인증을 시도하게 한다.
 
 		return httpSecurity.build();
@@ -67,7 +82,6 @@ public class SecurityConfig {
 	public CorsConfigurationSource corsConfigurationSource() {
 		final CorsConfiguration corsConfiguration = new CorsConfiguration();
 
-		//		corsConfiguration.setAllowedOrigins(List.of(FRONT_URL_PATTERN, SERVER_URL_PATTERN, FRONT_URL_PATTERN_SERVER));
 		corsConfiguration.setAllowedOriginPatterns(List.of("*"));
 		corsConfiguration.setAllowedMethods(Arrays.asList(POST, GET, DELETE, PUT, HEAD, OPTIONS));
 		corsConfiguration.setAllowedHeaders(List.of(ALLOW_PATTERN));
