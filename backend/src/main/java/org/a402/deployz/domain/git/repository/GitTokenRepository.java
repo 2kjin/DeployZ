b5880e505ac306035a3bfac87cbe3d5c2245cac5
alkwen0996 <alkwen0996@naver.com>
2023-05-12 15:57:11 +0900

    ⛏ fix: GitTokenRepository 클래스 추가

---
+++ backend/src/main/java/org/a402/deployz/domain/git/repository/GitTokenRepository.java
@@ -0,0 +1,10 @@
+package org.a402.deployz.domain.git.repository;
+
+import org.a402.deployz.domain.git.entity.GitToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface GitTokenRepository extends JpaRepository<GitToken, Long> {
+
+}
