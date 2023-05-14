c5f98f0e71ce5dbf45350a9b2cd3a27ea2378925
alkwen0996 <alkwen0996@naver.com>
2023-05-14 17:16:58 +0900

    ✏ typo fix: 폴더 경로 구분자 오타수정

--- backend/src/main/java/org/a402/deployz/domain/deploy/common/PathParser.java
+++ backend/src/main/java/org/a402/deployz/domain/deploy/common/PathParser.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class PathParser {
-	public static final String SEPERATOR = "/";
+	public static final String SEPARATOR = "/";
 	@Value("${deployz.rootPath}")
 	private String root;
 
@@ -19,22 +19,22 @@ public class PathParser {
 	}
 
 	public StringBuilder getProjectPath(final String projectName) {
-		return getRootPath().append(SEPERATOR).append(projectName);
+		return getRootPath().append(SEPARATOR).append(projectName);
 	}
 
 	public StringBuilder getLogPath(final String projectName) {
-		return getProjectPath(projectName).append(SEPERATOR).append(log);
+		return getProjectPath(projectName).append(SEPARATOR).append(log);
 	}
 
 	public StringBuilder getRepositoryPath(final String projectName, final String itemName,
 		final String repositoryName) {
-		return getProjectPath(projectName).append(SEPERATOR)
+		return getProjectPath(projectName).append(SEPARATOR)
 			.append(itemName)
-			.append(SEPERATOR)
+			.append(SEPARATOR)
 			.append(repositoryName);
 	}
 
 	public StringBuilder getItemPath(final String projectName, final String itemName) {
-		return getProjectPath(projectName).append(SEPERATOR).append(itemName);
+		return getProjectPath(projectName).append(SEPARATOR).append(itemName);
 	}
 }
