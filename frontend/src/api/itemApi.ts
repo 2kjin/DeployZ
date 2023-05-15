a9b53da304a6f8f851bc4f9f3a78cb89127e2b53
정상기 <jackid1103@naver.com>
2023-05-15 14:50:22 +0900

    ⛏ fix : 프로젝트 빌드 API 추가 & 빌드 로그 줄바꿈 처리

--- frontend/src/api/itemApi.ts
+++ frontend/src/api/itemApi.ts
@@ -1,9 +1,5 @@
-import { instance } from "./api";
-
-//아이템 상세 정보 보여주는 api -> itemDetail
-export const fetchItemDetail = async (containerIdx: number) => {
-  return instance.get(`/item/detail/${containerIdx}`);
-};
+import axios from "axios";
+import { instance, testInstance } from "./api";
 
 //빌드 시간 변경하기
 export const changeBuildTime = (value: string) => {
@@ -19,15 +15,25 @@ export const changeBuildTime = (value: string) => {
 
   // 년, 월, 일, 시, 분 구하기
   const year = korTs.getFullYear();
-  const month = korTs.getMonth() + 1;
-  const date = korTs.getDate();
-  const hour = korTs.getHours();
-  const minute = korTs.getMinutes();
-  const second = korTs.getSeconds();
+  const month =
+    korTs.getMonth() + 1 < 10
+      ? `0${korTs.getMonth() + 1}`
+      : korTs.getMonth() + 1;
+  const date = korTs.getDate() < 10 ? `0${korTs.getDate()}` : korTs.getDate();
+  const hour =
+    korTs.getHours() < 10 ? `0${korTs.getHours()}` : korTs.getHours();
+  const minute =
+    korTs.getMinutes() < 10 ? `0${korTs.getMinutes()}` : korTs.getMinutes();
+  const second =
+    korTs.getSeconds() < 10 ? `0${korTs.getSeconds()}` : korTs.getSeconds();
+  return `${year}.${month}.${date} ${hour}:${minute}:${second}`;
+};
 
-  if (hour <= 9) {
-    return `${year}.${month}.${date} 0${hour}:${minute}:${second}`;
-  } else {
-    return `${year}.${month}.${date} ${hour}:${minute}:${second}`;
-  }
+//아이템 상세 정보 보여주는 api -> itemDetail
+export const fetchItemDetail = async (containerIdx: number) => {
+  return instance.get(`/item/detail/${containerIdx}`);
+};
+
+export const requestDeploy = async (itemIdx: number) => {
+  return testInstance.post(`/deploy/${itemIdx}`);
 };
