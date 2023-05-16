eff4d7c73aca1df9fa88481fb3df4ba21d8e73aa
alkwen0996 <alkwen0996@naver.com>
2023-05-16 14:41:56 +0900

    💡 refactor: nginx deleteflag 필드 추가

--- backend/src/main/java/org/a402/deployz/domain/project/entity/NginxConfig.java
+++ backend/src/main/java/org/a402/deployz/domain/project/entity/NginxConfig.java
@@ -1,12 +1,25 @@
 package org.a402.deployz.domain.project.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.ColumnDefault;
+
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
-import javax.persistence.*;
-import java.util.ArrayList;
-import java.util.List;
 
 @Entity
 @Getter
@@ -28,15 +41,20 @@ public class NginxConfig {
     private String sslCertificate;
     @Column(name = "ssl_certificate_key", length = 100)
     private String sslCertificateKey;
+    @ColumnDefault("false")
+    @Column(name = "deleted_flag", nullable = false)
+    private boolean deletedFlag;
 
     @Builder
     public NginxConfig(final Long idx, final Project project, final List<ProxyConfig> proxyConfigs,
-                   final String domain, final String sslCertificate, final String sslCertificateKey) {
+                   final String domain, final String sslCertificate, final String sslCertificateKey,
+        final boolean deletedFlag) {
         this.idx = idx;
         this.project = project;
         this.proxyConfigs = proxyConfigs;
         this.domain = domain;
         this.sslCertificate = sslCertificate;
         this.sslCertificateKey = sslCertificateKey;
+        this.deletedFlag = deletedFlag;
     }
 }
